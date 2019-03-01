600de2fc22b8fe7dd8a72ae67edf9494b1df6c0d
robert <24939343+engsterhold@users.noreply.github.com>
2019-03-01 11:32:19 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='REDE-IIIF-Manifest',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     license='',
     long_description=open('README.md').read(),
